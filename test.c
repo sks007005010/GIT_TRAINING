92713e593923e4c92ac25dcea8db684c811c76d9
Sitesh <Sitesh@email.com>
2020-07-28 15:24:30 +0530

    Sitesh mergde conflic

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main(){
 	printf("Hello World 2\n");
 	printf("Hello World 2\n");
 	printf("Hello World 2\n");
-	printf("Hello World 2\n");
+	./nnldvnmdsnvm,~printf("Hello World 2\n");
 	printf("s-brabch ~Hello World 2\n");
 	printf("Hello World 2\n");
 	printf("Hello World 2\n");
